487064d508948271564bbb181a60ec2ca73d3643
盛中柱 <1651260742@qq.com>
2024-01-18 17:39:35 +0800

    s

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import 'element-plus/es/components/loading/style/css';
 import './style.css'
+import './output.css'
 import App from './App.vue'
 import store from './store';
 
